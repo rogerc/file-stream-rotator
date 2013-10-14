7eda626dbed3b7b6f33779f4dbc57249a880ebb7
Viktor Trako <viktor.trako@btinternet.com>
2013-10-14 16:34:17 +0100

    rotate files at a given minute or hourly interval

--- FileStreamRotator.js
+++ FileStreamRotator.js
@@ -8,6 +8,8 @@
  * Module dependencies.
  */
 var fs = require('fs');
+var moment = require('moment');
+var DATE_FORMAT = ('YYYYMMDDHHmm');
 
 /**
  * FileStreamRotator:
@@ -32,35 +34,101 @@ var fs = require('fs');
  */
 
 module.exports = FileStreamRotator = {};
+var staticFrequency = ['daily', 'test', 'm', 'h'];
+
+var _checkNumAndType = function(type, num) {
+  if(typeof num == 'number') {
+    switch (type) {
+      case 'm':
+        if(num < 0 || num > 60) {
+          return false;
+        }
+        break;
+      case 'h':
+        if(num < 0 || num > 24) {
+          return false;
+        }
+        break;
+    }
+    return {type: type, digit: num};
+  }
+}
+
+var _checkDailyAndTest = function(freqType) {
+    switch(freqType) {
+        case 'daily':
+          return {type: freqType, digit: undefined};
+          break;
+        case 'test':
+          return {type: freqType, digit: 0};
+    }
+    return false;
+}
+
+FileStreamRotator.getFrequency = function(frequency) {
+    for(var i = 0; i < staticFrequency.length; i++) {
+        var type = staticFrequency[i];
+        var regex = frequency.toLowerCase().match(type);
+        if(regex && Array.isArray(regex) && regex.length > 0) {
+            var freqType = regex[0];
+
+            var dailyOrTest = _checkDailyAndTest(freqType);
+            if(dailyOrTest) {
+              return dailyOrTest;
+            }
+
+            var numRegex = frequency.match(/(.*)[A-Za-z]/);
+            if(numRegex && Array.isArray(numRegex) && numRegex.length >= 1 && numRegex[1] !== '') {
+                var num = numRegex[1]/1;
+                return _checkNumAndType(type, num);
+            } else {
+                return false;
+            }
+        } else { //no match so return false once reaching the end of the loop
+            if(i == staticFrequency.length-1) {
+                return false;
+            }
+        }
+    }
+}
+
+FileStreamRotator.getDate = function(format) {
+    var date = new Date();
+
+    if(staticFrequency.indexOf(format.type) !== -1 && format.type !== 'daily' ) {
+
+        switch(format.type) {
+            case 'm':
+                return moment().add('m', format.digit).format(DATE_FORMAT);
+                break;
+            case 'h':
+                return moment().add('h', format.digit).format(DATE_FORMAT);
+                break;
+            case 'test':
+                return moment().format(DATE_FORMAT);
+        }
+
+    }
+    return moment().format('YYYYMMDD');
+}
 
 FileStreamRotator.getStream = function(options){
 
-    function padNumber(number){
-        if( number.toString().length < 2){
-            return  "0" + number;
-        }
-        return number;
-    }
-
-    function getDate(format){
-        var date = new Date();
-        switch(format){
-            case 'test':
-                return ( date.getFullYear() + "" + padNumber((date.getMonth()+1)) + "" + padNumber(date.getDate()) +
-                    "" + padNumber(date.getHours()) + "" + padNumber(date.getMinutes()) )
-                    ;
-            default:
-                return date.getFullYear() + "" + padNumber((date.getMonth()+1)) + "" + padNumber(date.getDate());
-
-        }
-    }
-
+    var frequencyMetaData = null;
+    var curDate = null
     if(!options.filename){
         console.error("No filename supplied. Defaulting to STDOUT");
         return process.stdout;
     }
-    
-    var curDate = (options.frequency?getDate(options.frequency):"");
+
+    if(options.frequency) {
+        frequencyMetaData = this.getFrequency(options.frequency);
+    }
+
+    if(frequencyMetaData) {
+        curDate = (options.frequency?this.getDate(frequencyMetaData):"");
+    }
+
     var filename = options.filename;
     var logfile = filename + (curDate?"." + curDate:"");
     var verbose = (options.verbose !== undefined?options.verbose:true);
@@ -68,33 +136,31 @@ FileStreamRotator.getStream = function(options){
         console.log("Logging to " + logfile);
     }
     var rotateStream = fs.createWriteStream(logfile, {flags: 'a'});
-    switch(options.frequency){
-        case 'test':
-        case 'daily':
-            if(verbose){
-                console.log("Rotating file " + options.frequency);
-            }
-            var stream = {end: rotateStream.end};
-            stream.write = (function(str,encoding){
-                var newDate = getDate(options.frequency);
-                if(newDate != curDate){
-                    var newLogfile = filename + (curDate?"." + newDate:"");
-                    if(verbose){
-                        console.log("Changing logs from " + logfile+ " to " + newLogfile);
-                    }
-                    curDate = newDate;
-                    logfile = newLogfile;
-                    rotateStream.destroy();
-                    rotateStream = fs.createWriteStream(newLogfile, {flags: 'a'});
+    if(options.frequency == 'daily' ||  options.frequency== 'h' || options.frequency == 'm') {
+        if(verbose){
+            console.log("Rotating file " + options.frequency);
+        }
+        var stream = {end: rotateStream.end};
+        stream.write = (function(str,encoding){
+            var newDate = getDate(frequencyMetaData);
+            if(newDate != curDate){
+                var newLogfile = filename + (curDate?"." + newDate:"");
+                if(verbose){
+                    console.log("Changing logs from " + logfile+ " to " + newLogfile);
                 }
-                rotateStream.write(str,encoding);
-
-            }).bind(this);
-            return stream;
-        default:
-            if(verbose){
-                console.log("File won't be rotated");
+                curDate = newDate;
+                logfile = newLogfile;
+                rotateStream.destroy();
+                rotateStream = fs.createWriteStream(newLogfile, {flags: 'a'});
             }
-            return rotateStream;
+            rotateStream.write(str,encoding);
+
+        }).bind(this);
+        return stream;
+    } else {
+        if(verbose){
+            console.log("File won't be rotated");
+        }
+        return rotateStream;
     }
 }
